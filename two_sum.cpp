70e457891b06a588a4efd0ae31c6959851a0f79c
tamilvanan31 <57147424+tamilvananb31@users.noreply.github.com>
2021-10-09 14:33:00 +0530

    Create two_sum.cpp

---
+++ two_sum.cpp
@@ -0,0 +1,48 @@
+/*
+1. Two Sum
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+Solution
+Idea behind this solution is, create a hash map and search whether the value (target - nums[i]) exist in the map, else add the index to the value.
+In this way we will get the solution.
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> search;
+        
+        for(int i = 0; i < nums.size(); ++i) {
+            if(search.find(target - nums[i]) != search.end()) 
+                return {search[target - nums[i]], i};
+            search[nums[i]] = i;
+        }
+        return {};    
+    }
+};

1c443baedfec8172e36eaaa2eb93370f9633124e
tamilvanan31 <57147424+tamilvananb31@users.noreply.github.com>
2021-10-12 12:47:06 +0530

    Update 2_repeat_and_missing.cpp

--- 2_repeat_and_missing.cpp
+++ 2_repeat_and_missing.cpp
@@ -1,8 +1,7 @@
-There are certain problems which are asked in the interview to also check how you take care of overflows in your problem.
+There are certain problems which are asked in the interview to also check how you take care of overflows in your problem. This is one of those problems.
 
-This is one of those problems.
-
-Please take extra care to make sure that you are type-casting your ints to long properly and at all places. Try to verify if your solution works if number of elements is as large as 105
+Please take extra care to make sure that you are type-casting your ints to long properly and at all places. 
+Try to verify if your solution works if number of elements is as large as 105
 
 Food for thought :
 
